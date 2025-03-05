ea5af49a569633ef20696c01cce24264d749fc1c
joedvn <joedemarrosero.1724@gmail.com>
2025-03-05 12:04:12 +0800

    first commit

---
+++ database/factories/PositionModelFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\positionModel>
+ */
+class PositionModelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
